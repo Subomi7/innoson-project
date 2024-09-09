96e7f23936c53ecfe8a37dff925ced30b900217f
Oluwatobiloba Odeyemi <petertob01@gmail.com>
2024-09-09 14:55:06 +0100

    worked on hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
+import Hero from './components/Hero';
 import Navbar from './layout/Navbar';
 
 function App() {
-  return <>
-  <Navbar/>
-  </>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../styles/Hero.css"
+
+const Hero = () => {
+  return (
+    <>
+    <main>
+        <h2 className='container text-white'></h2>
+    </main>
+    </>
+)
+}
+
+export default Hero
