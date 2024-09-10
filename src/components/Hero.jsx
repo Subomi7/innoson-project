f5e3dcee0255df4e1272d79a1049a572cf9242e5
Oluwatobiloba Odeyemi <petertob01@gmail.com>
2024-09-10 16:44:21 +0100

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,17 +5,19 @@ const Hero = () => {
   return (
     <>
       <main className='hero-section d-flex flex-column align-items-start justify-content-center'>
-        <h2 className='container'>
-          Crafting Africa's <br /> Roads Ahead
-        </h2>
-        <p className='container'>
-          Our vehicle assembly line represents the essence of African
-          <br />
-          innovation. From rugged off-road explorers to sleek city cruisers,
-          <br /> we assemble vehicles that traverse the varied landscapes of our{' '}
-          <br /> beautiful continent
-        </p>
         <div className='overlay-div'></div>
+        <div className='container text-content'>
+          <h2 className='container'>
+            Crafting Africa's <br /> Roads Ahead
+          </h2>
+          <p className='container'>
+            Our vehicle assembly line represents the essence of African
+            <br />
+            innovation. From rugged off-road explorers to sleek city cruisers,
+            <br /> we assemble vehicles that traverse the varied landscapes of
+            our <br /> beautiful continent
+          </p>
+        </div>
       </main>
     </>
   );
